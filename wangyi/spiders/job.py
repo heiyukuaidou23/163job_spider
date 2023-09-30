910db199cb9b392b4c270d8fe0d39288d2781acd
heiyukuaidou23 <2865488943@qq.com>
2023-09-30 15:24:59 +0800

    金秋雨落旱逢甘

---
+++ wangyi/spiders/job.py
@@ -0,0 +1,34 @@
+import scrapy
+from wangyi.items import WangyiItem
+
+class JobSpider(scrapy.Spider):
+    name = "job"
+    allowed_domains = ["163.com"]
+    start_urls = ["https://hr.163.com/position/list.do"]
+
+    def parse(self, response):
+        # 1.提取一页的数据
+        trs = response.xpath('//*[@class="position-tb"]/tbody/tr')
+        # print(len(trs))
+        for num, tr in enumerate(trs):
+            # 设置一个过滤条件
+            if num % 2 == 0:
+                item = WangyiItem()
+                item['name'] = tr.xpath('./td[1]/a/text()').extract_first()
+                item['link'] = response.urljoin(tr.xpath('./td[1]/a/@href').extract_first())
+                item['depart'] = tr.xpath('./td[2]/text()').extract_first()
+                item['category'] = tr.xpath('./td[3]/text()').extract_first()
+                item['type'] = tr.xpath('./td[4]/text()').extract_first()
+                item['address'] = tr.xpath('./td[5]/text()').extract_first()
+                item['num'] = tr.xpath('./td[6]/text()').extract_first().strip()
+                item['date'] = tr.xpath('./td[7]/text()').extract_first()
+                yield item
+        # 2.进行翻页操作
+        part_url = response.xpath('/html/body/div[2]/div[2]/div[2]/div/a[last()]/@href').extract_first()
+        # 判断终止条件
+        if part_url != 'javascript:void(0)':
+            next_url = response.urljoin(part_url)
+            yield scrapy.Request(
+                url=next_url,
+                callback=self.parse
+            )
